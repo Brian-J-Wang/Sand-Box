ef469f4d37d591e1497d41dae18ebd8f28e82a91
Brian Wang <brianwang7102@gmail.com>
2025-01-21 02:45:08 -0500

    feat: added canvas

---
+++ src/components/particle/particle.ts
@@ -0,0 +1,14 @@
+export default class Particle {
+    constructor() {}
+
+    //returns relative position of where the particle wants to go,
+    //and update's the particle if 
+    calculateNextStep() {
+
+    }
+
+    //
+    resolve() {
+
+    }
+}
---
+++ src/components/sandbox.ts
@@ -0,0 +1,22 @@
+import p5 from "p5";
+
+export default class SandBox {
+    constructor(private readonly canvas: string) {}
+
+    start() {
+        new p5((p: p5) => {
+            p.setup = () => {
+                p.createCanvas(1000, 1000).parent(this.canvas);
+                p.frameRate(24);
+            }
+
+            p.draw = () => {
+                this.step(p);
+            };
+        });
+    }
+
+    step(p: p5) {
+        p.circle(50, 50, 10);
+    }
+}
