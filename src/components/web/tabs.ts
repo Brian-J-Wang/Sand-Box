10ed0ca8ee346c471668cb070e2f52ebd0b0478d
Brian Wang <brianwang7102@gmail.com>
2025-01-25 22:39:38 -0500

    feat: added tabs switching

--- src/components/web/tabs.ts
+++ src/components/web/tabs.ts
@@ -6,6 +6,38 @@ const Tabs = {
 
     },
     Gas: {
-        
+
+    }
+}
+
+export class TabsHandler {
+    tabs: HTMLElement;
+    tabButtons: Element[] = [];
+    onTabsChanged: ((id: string) => void)[] = [];
+    constructor(string: string, defaultSelected: string) {
+        this.tabs = document.querySelector(string)!;
+
+        this.tabs.querySelectorAll('.tabs__button').forEach((tab) => {
+            this.tabButtons.push(tab);
+            (tab as HTMLButtonElement).addEventListener('click', () => {
+                this.setActiveButton(tab.id);
+            })
+        })
+
+        this.setActiveButton(defaultSelected);
+    }
+
+    setActiveButton(id: string) {
+        this.tabButtons.forEach((button) => {
+            if (button.id == id) {
+                button.classList.add("tabs__button_selected");
+            } else {
+                button.classList.remove("tabs__button_selected");
+            }
+        });
+
+        this.onTabsChanged.forEach((fn) => {
+            fn(id);
+        })
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import SandBox from "./components/sandbox";
+import { TabsHandler } from "./components/web/tabs";
 
 const sandBox = new SandBox('sandbox');
 sandBox.start();
 
+const tabs = new TabsHandler('#tabs', 'tab-btn__solid');
